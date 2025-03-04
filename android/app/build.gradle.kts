93d49d094f1275867f756ad4b6579fb7022e034d
Afsaumutoniwase <a.umutoniwa@alustudent.com>
2025-03-05 00:07:21 +0200

    creating project structure

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services") // Firebase plugin
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.jaclean"
+    ndkVersion = "27.0.12077973"
+    compileSdk = flutter.compileSdkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.jaclean"
+        minSdk = 23 // Ensure minSdk is not duplicated
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug") // Temporary for debug builds
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/jaclean/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jaclean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
