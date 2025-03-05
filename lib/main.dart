c6ce3bffaab059a9feb4680ab1b99617ed40d675
Afsaumutoniwase <a.umutoniwa@alustudent.com>
2025-03-05 10:30:07 +0200

    modifying the structure

--- lib/main.dart
+++ lib/main.dart
@@ -51,18 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
