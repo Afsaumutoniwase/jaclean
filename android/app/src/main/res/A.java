55ca92feb700f5693dbfbdca73cebe53d6f33091
o-keita <o.keita@alustudent.com>
2025-04-01 18:56:03 +0000

    added icon

---
+++ android/app/src/main/res/A.java
@@ -0,0 +1,5 @@
+/home/omar/Downloads/AppIcons (2)/android/mipmap-hdpi
+/home/omar/Downloads/AppIcons (2)/android/mipmap-mdpi
+/home/omar/Downloads/AppIcons (2)/android/mipmap-xhdpi
+/home/omar/Downloads/AppIcons (2)/android/mipmap-xxhdpi
+/home/omar/Downloads/AppIcons (2)/android/mipmap-xxxhdpi
